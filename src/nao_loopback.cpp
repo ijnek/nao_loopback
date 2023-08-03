28a73d928565fb684b719715fa25c4742ddae928
ijnek <kenjibrameld@gmail.com>
2023-08-03 02:07:17 +0000

    fix tests
    Signed-off-by: ijnek <kenjibrameld@gmail.com>

--- src/nao_loopback.cpp
+++ src/nao_loopback.cpp
@@ -1,3 +1,17 @@
+// Copyright 2023 Kenji Brameld
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "nao_loopback/nao_loopback.hpp"
 
 #include <chrono>
@@ -18,7 +32,6 @@ NaoLoopback::NaoLoopback(const rclcpp::NodeOptions & options)
     create_subscription<nao_command_msgs::msg::JointPositions>(
     "effectors/joint_positions", 10,
     [this](nao_command_msgs::msg::JointPositions::SharedPtr cmd) {
-
       if (cmd->indexes.size() != cmd->positions.size()) {
         RCLCPP_ERROR(
           get_logger(),
